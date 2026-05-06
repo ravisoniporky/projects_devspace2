01958a541a3946b69bb9b6a00217772b6279490b
ravisoniporky <ravi.soni@porky.com>
2026-05-06 14:26:03 +0000

    comm

---
+++ zattritionbas/webapp/Component.js
@@ -0,0 +1,12 @@
+sap.ui.define(
+    ["sap/suite/ui/generic/template/lib/AppComponent"],
+    function (Component) {
+        "use strict";
+
+        return Component.extend("customer.porky.zattritionbas.Component", {
+            metadata: {
+                manifest: "json"
+            }
+        });
+    }
+);
